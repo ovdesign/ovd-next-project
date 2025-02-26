2c5aa1d7c7cb9dccbd67d3574587e09e6e916442
ovdesign <koizumi@ovdesign.jp>
2025-02-26 16:34:50 +0900

    2/26

--- app/page.tsx
+++ app/page.tsx
@@ -55,39 +55,21 @@ export default function Home() {
       <h1 className={styles.title}>Time machine is a dream device.</h1>
       <p className={styles.description}>この世界のどこかでタイムマシーンみたいなユメの装置を開発しているとしたら？</p>
     </div>
-      <Image
-        className={styles.bgimg}
-        src="/img-mv.jpg"
-        alt=""
-        width={4000}
-        height={1200}
-      />
-      </section>
-      <section className={styles.news}>
-        <h2 className={styles.news}>News</h2>
+      <Image className={styles.bgimg} src="/img-mv.jpg" alt="" width={4000} height={1200} />
+  </section>
+  <section className={styles.news}>
+    <h2 className={styles.newsTitle}>News</h2>
         <ul>
           {sliceData.map((article) => (
-            <li key={article.id} className={styles.list}>
+            <li key={`news-${article.id}`} className={styles.list}>
               <div className={styles.link}>
-                <Image
-                  className={styles.image}
-                  src="/no-image.png"
-                  alt="No Image"
-                  width={1200}
-                  height={630}
-                />
+                <Image className={styles.image} src="/no-image.png" alt="No Image" width={1200} height={630} />
                 <dl className={styles.contents}>
                   <dt className={styles.newsItemTitle}>{article.title}</dt>
                   <dd className={styles.meta}>
                     <span className={styles.tag}>{article.category.name}</span>
                     <span className={styles.date}>
-                      <Image
-                        src="/clock.svg"
-                        alt=""
-                        width={16}
-                        height={16}
-                        priority
-                      />
+                      <Image src="/clock.svg" alt="" width={16} height={16} />
                       {article.publishedAt}
                     </span>
                   </dd>
@@ -96,7 +78,7 @@ export default function Home() {
             </li>
           ))}
         </ul>
-      </section>
+  </section>
     </>
   );
 }
